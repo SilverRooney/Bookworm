094f20b9a9bfc76515d592e4b2cf2340b6d2588f
SilverRooney <silver.rooney.dev@gmail.com>
2022-07-03 12:12:12 -0700

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Silver on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
